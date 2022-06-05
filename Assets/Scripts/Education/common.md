baf232b3a6af5ce1ac5db69103b9dd2ebdfb4ea7
kartemake <a.kuskov@make.st>
2022-06-05 16:36:34 +0700

    update education

--- Assets/Scripts/Education/common.md
+++ Assets/Scripts/Education/common.md
@@ -1,7 +1,7 @@
 using System;
 # Общая информация
 ## Переменные
-private string name; - показана не будет
+  private string name; - показана не будет
 
 ### Если переменная была privat и вы хотим её показать
   [Serializable] - работает при подключении System
@@ -14,8 +14,19 @@ private string name; - показана не будет
   [NonSerialized] - работает при подключении System
   public string name; - показана не будет
 
-  
-  
-  
+## Работа с игровым объектом
+  ### Работа с компонентами
+  <blockquote>
+    <p>Компонент позволяет получить данные о позиции и вращении и размере игрового объекта.</p>
+    <p>Также можно у компонента получить игровой объект через ".gameObject": transform[i].gameObject.</p>
+    <p>Возможно получить имя объекта через ".name": transform[i].gameObject.name</p>
+  </blockquote>
 
-  
+  <h4>Компонент Transform:</h4>
+  <pre>
+    <ul>
+      <li>Position: GameObject.GetComponent<Transform>().position = new Vector3(10, 20, 30);</li>
+      <li>Rotation: GameObject.GetComponent<Transform>().rotation = Quaternion.Euler(0,0,0);</li>
+      <li>Scale: GameObject.GetComponent<Transform>().scale = new Vector3(10, 20, 30); - не верно</li>
+    </ul>
+  </pre>
--- Assets/Scripts/GameObject/Basics.cs
+++ Assets/Scripts/GameObject/Basics.cs
@@ -6,16 +6,57 @@ using System;
 public class Basics : MonoBehaviour
 {
 
-    public GameObject cube;
+// массив игровых объектов
+  public GameObject[] arCube = new GameObject[3];
+  public Transform[] transforms = new Transform[3];
+  public GameObject cube;
+  public Transform transformCube;
 
-    public void Start() {
-        cube.SetActive(false);
+  public Light _light; 
+
+  public void Start() {
+    // cube.SetActive(false);
+    
+    // обращение к компоненту объекта
+    // cube.GetComponent<Transform>().position = new Vector3(10, 20, 30);
+  }
+
+  float x;
+  
+  async void Update () {
+    x += Time.deltaTime * 10;
+      
+    for(int i = 0; i < transforms.Length; i++ ) {
+      transforms[i].rotation = Quaternion.Euler(x,x*1.2f,0);
     }
-    
-//     // метод при удалении объекта на который скрипт повешен
-//     public void OnDestroy()
-//     {
-//        Debug.Log("OnDestroy");
-//     }
-    
+
+    // cube.GetComponent<Transform>().rotation = Quaternion.Euler(x,0,0);
+
+    transformCube.rotation = Quaternion.Euler(x,0,0);
+
+    timeDay();
+  }
+
+
+  float intensity = 0;
+  bool direction = true;
+  public void timeDay() {
+    // от 0 до 1
+    if ( intensity <= 0 ) {
+      direction = true;
+    }
+
+    // от 1 до 0
+    if ( intensity >= 1 ) {
+      direction = false;
+    } 
+
+    if ( direction ) {
+      intensity += Time.deltaTime / 10;
+    } else {
+      intensity -= Time.deltaTime / 10;
+    }
+
+    _light.intensity = intensity;
+  }
 }
