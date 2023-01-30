241c8083636223f1074defb27e2614c64826da8c
kartemake <a.kuskov@make.st>
2023-01-30 14:04:46 +0700

    added script GlobalVariables

---
+++ Assets/Scripts/Global/GlobalVariables.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalVariables : MonoBehaviour
+{
+    // Global variables user state
+    public bool AUTH_USER = false;
+    public string VERIFICATION_MODE = "";
+    public int USER_BALLS = 0;
+
+
+    // Global variables window
+    public GameObject PREV_WINDOW;
+    public GameObject ACTIVE_WINDOW;
+    public GameObject LC_WINDOW;
+}
