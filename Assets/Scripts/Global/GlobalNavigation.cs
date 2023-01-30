80f1c529077490843166828175c5e8c6f45496d7
kartemake <a.kuskov@make.st>
2023-01-30 14:05:04 +0700

    added script GlobalNavigation

---
+++ Assets/Scripts/Global/GlobalNavigation.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// File includes methods for global replace: variables and history navigation
+
+public class GlobalNavigation : MonoBehaviour
+{
+    public GlobalVariables GlobalVariables;
+
+    public void Back()
+    {
+        CloseActiveWindow();
+        GlobalVariables.PREV_WINDOW.SetActive(true);
+
+        ReplaceGlobalVariablesWindow(GlobalVariables.PREV_WINDOW);
+    }
+
+    public void ReplaceGlobalVariablesWindow(GameObject activeWindow)
+    {
+        GlobalVariables.PREV_WINDOW = GlobalVariables.ACTIVE_WINDOW;
+        GlobalVariables.ACTIVE_WINDOW = activeWindow;
+    }
+
+    public void ReplaceGlobalVariablesVerificationMode(string verificationMode)
+    {
+        GlobalVariables.VERIFICATION_MODE = verificationMode;
+    }
+
+    public void CloseActiveWindow()
+    {
+        GlobalVariables.ACTIVE_WINDOW.SetActive(false);
+    }
+
+    public void OpenNextWindow(GameObject window)
+    {
+        window.SetActive(true);
+    }
+}
