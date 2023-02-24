53f10ee23dff1e2aaec3a679ffb8f1e6104a66ae
kartemake <a.kuskov@make.st>
2023-02-24 21:40:02 +0700

    edited DoorAnimation

--- Assets/Scripts/Enviroment/Door/DoorAnimation.cs
+++ Assets/Scripts/Enviroment/Door/DoorAnimation.cs
@@ -8,43 +8,26 @@ public class DoorAnimation : MonoBehaviour
     public GameObject Text;
     bool doorIsOpened = false;
     private Animation doorAnimation;
-    private void Start()
+    void Start()
     {
         doorAnimation = GetComponent<Animation>();
-        doorAnimation["open__door"].speed = speedAnimation;
+        doorAnimation["open_door_one"].speed = speedAnimation;
+        doorAnimation["close_door_one"].speed = speedAnimation;
 
     }
-    private void Update()
+    void Update()
     {
-        if (doorAnimation.isPlaying)
-        {
-            return;
-        }
+        if (doorAnimation.isPlaying) return;
 
         if (Input.GetKeyUp(KeyCode.F))
         {
-
-            if (doorIsOpened == false)
-            {
-                doorAnimation.Play("open__door");
-                foreach (AnimationState state in doorAnimation)
-                {
-                    print(state.speed);
-                }
-
-            }
-            if (doorIsOpened == true)
-            {
-                doorAnimation.Play("close__door");
-            }
-
+            string animateName = doorIsOpened ? "open_door_one" : "close_door_one";
+            doorAnimation.Play(animateName);
             doorIsOpened = !doorIsOpened;
         }
     }
     void OnTriggerEnter(Collider other)
     {
-        Text.SetActive(true);
-        Debug.Log(123);
     }
     void OnTriggerExit(Collider other)
     {
