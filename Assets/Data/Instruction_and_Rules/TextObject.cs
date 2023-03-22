f79867cb497b3b6be459925c3461884374712d03
kartemake <a.kuskov@make.st>
2023-03-22 22:56:37 +0700

    added Data Scriptables for Instruction and Rules

---
+++ Assets/Data/Instruction_and_Rules/TextObject.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewTextData", menuName = "Create TextData")]
+public class TextObject : ScriptableObject
+{
+    public int id;
+    public string titleBig;
+    public string[] titlesSmall;
+    public string[] paragraphs;
+    public bool pass;
+
+    public int Id { get => id; }
+    public string TitleBig { get => titleBig; }
+    public string[] TitlesSmall { get => titlesSmall; }
+    public string[] Paragraphs { get => paragraphs; }
+    public bool Pass { get => pass; }
+}
